23892800393d3c0453e31d0d0c4455fdd0225ff8
Ushakov Michael <um.nix.user@gmail.com>
2019-01-26 02:55:44 +0400

    365 and 366 were also tested

--- ISA_Camac_Interface/sm2201_interface_board/lib/SN74LS365.v
+++ ISA_Camac_Interface/sm2201_interface_board/lib/SN74LS365.v
@@ -9,17 +9,21 @@
 // Project Name:   sm2201_interface_board
 // Target Devices: 
 // Tool versions:  Quartus 12.1 sp1
-// Description:    An implementation of chip SN74LS365 (3 state HEX buffers)
+// Description:    An implementation of chip SN74LS365 / 366 (buffers /inverters)
+//                 365 if INVERTED_OUTPUT is 0, 366 if INVERTED_OUTPUT is 1
 //
 // Dependencies: 
 //
 // Revision: 
 // Revision 1.0
-// Additional Comments: Analog of К555ЛП10
+// Additional Comments: Analog of К555ЛП10 / ЛН6
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-module SN74LS365
+module SN74LS365 #
+(
+    INVERTED_OUTPUT = 0
+)
 (
     input wire e1,
     input wire e2,
@@ -28,6 +32,8 @@ module SN74LS365
 );
 
 wire ena = e1 | e2;
-assign out = ena = 1'b0 ? data : 6'bZ;
+wire [5:0] data_res;
+assign data_res = INVERTED_OUTPUT == 0 ? data : ~data;
+assign out = ena == 1'b0 ? data_res : 6'bZ;
 
 endmodule
---
+++ ISA_Camac_Interface/sm2201_interface_board/tests/SN74LS366A_testbench.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   02:52:45 01/26/2019
+// Design Name:   SN74LS365
+// Module Name:   E:/PLD/Sm2201/ISA_Camac_Interface/sm2201_interface_board/tests/SN74LS366A_testbench.v
+// Project Name:  sm2201_interface_board
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: SN74LS365
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module SN74LS366A_testbench;
+
+	// Inputs
+	reg e1;
+	reg e2;
+	reg [5:0] data;
+
+	// Outputs
+	wire [5:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	SN74LS365 # (.INVERTED_OUTPUT(1)) uut (
+		.e1(e1), 
+		.e2(e2), 
+		.data(data), 
+		.out(out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		e1 = 1;
+		e2 = 0;
+		data = 0;
+		
+		#100;
+		e2 = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;      
+		// Add stimulus here
+      e1 = 0;
+		
+		#100;
+		e2 = 0;
+	end
+      
+	always
+	begin
+	    #100 data <= data + 1;
+	end
+      
+endmodule
+
