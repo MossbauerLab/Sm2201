2e220006d015652a01346f540ef992f6af558c8c
Ushakov Michael <um.nix.user@gmail.com>
2019-06-25 23:27:37 +0300

    intermediate: attempt to access to  ISA and CAMAC

--- ISA_Camac_Interface/sm2201_interface_board/sm2201_interface_board.v
+++ ISA_Camac_Interface/sm2201_interface_board/sm2201_interface_board.v
@@ -19,8 +19,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module sm2201_interface_board(
-    input wire[7:0] isa_bus_data,
-	 output wire [7:0] camac_bus
+    input wire[9:0] isa_bus_data,
+	 input wire isa_bus_ale,
+	 input wire isa_bus_aen,
+	 input wire isa_bus_clk,
+	 input wire isa_bus_chrdy,
+	 input wire isa_bus_reset,
+	 input wire isa_bus_ior,
+	 input wire isa_bus_iow,
+	 output wire [7:0] isa_bus_addr,
+	 output wire [4:0] isa_bus_irq,
+	 input wire [15:0] camac_data_bus
+	 output wire [11:0] camac_addr_bus;
+	 // todo: umv: implement connection to CAMAC 
 );
 
 endmodule
