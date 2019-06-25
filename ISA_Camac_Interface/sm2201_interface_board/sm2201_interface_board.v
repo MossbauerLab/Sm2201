8646e7377738ed636a6c0a0fdfb4b6f682b961c7
Ushakov Michael <um.nix.user@gmail.com>
2019-06-25 21:52:26 +0300

    quartus project, that should aggregate all modules

---
+++ ISA_Camac_Interface/sm2201_interface_board/sm2201_interface_board.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:        MossbauerLab, EasySoft
+// Engineer:       EvilLord666 (Ushakov MV)
+// 
+// Create Date:    23:57:05 08/28/2017 
+// Design Name: 
+// Module Name:    sm2201_interface_board
+// Project Name:   sm2201_interface_board
+// Target Devices: 
+// Tool versions:  Quartus 19.1 sp1
+// Description:    
+//
+// Dependencies: 
+//
+// Revision: 1.0
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module sm2201_interface_board(
+    input wire[7:0] isa_bus_data,
+	 output wire [7:0] camac_bus
+);
+
+endmodule
